1f41e5ead1d8ae170045872ed475fedc12e09f3c
Mehareesh Meruga <mehareeshm@gmail.com>
2023-01-24 11:46:54 +0530

    Register and unregister broadcast in the MainActivity

--- app/src/main/java/com/meruga/geolocation/MainActivity.kt
+++ app/src/main/java/com/meruga/geolocation/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.meruga.geolocation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.IntentFilter
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var geoLocationReceiver: GeoLocationBroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        geoLocationReceiver = GeoLocationBroadcastReceiver()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(
+            geoLocationReceiver,
+            IntentFilter("android.location.PROVIDERS_CHANGED")
+        )
+    }
+
+    fun geoLocationON() {
+        GeoLocationBroadcastReceiver.isFirstResponse = false
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(geoLocationReceiver)
     }
 }
